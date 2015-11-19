60c0ffc0708ee170e3dbf1f1fd27e54fabc32b3c
Maroš Šeleng <marosseleng@gmail.com>
2015-11-19 16:46:38 +0100

    Add ngCordova. Add upload options screen.

--- www/js/app.js
+++ www/js/app.js
@@ -7,28 +7,34 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('symphonia', ['ionic', 'symphonia.controllers', 'symphonia.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/main',
+        templateUrl: 'templates/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('options', {
+        url: '/options',
+        templateUrl: 'templates/options.html',
+        controller: 'OptionsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/main');
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('main', {
-    url: '/main',
-    templateUrl: 'templates/main.html',
-    controller: 'MainCtrl'
-  });
-
-  $urlRouterProvider.otherwise('/main');
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,27 @@
 angular.module('symphonia.controllers', [])
 
-.controller('MainCtrl', function() {
+  .controller('MainCtrl', function () {
+
+  })
+  .controller('OptionsCtrl', function ($scope, $ionicLoading, $timeout) {
+    $scope.outputFormatList = [
+      {text: 'Music XML', value: 'mxl'},
+      {text: 'PDF', value: 'pdf'}
+    ];
+
+    $scope.data = {
+      clientSide: 'mxl'
+    };
+
+    $scope.show = function () {
+      $ionicLoading.show({
+        template: '<div class="loader"><svg class="circular">' +
+        '<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>' +
+        '</svg></div>'
+      });
+      $timeout(function () {
+        $ionicLoading.hide();
+      }, 2000);
+    };
 
   });
