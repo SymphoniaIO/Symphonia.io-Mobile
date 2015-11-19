06925750ef44794d844c02a64b9b7a646b9cbf83
Maroš Šeleng <marosseleng@gmail.com>
2015-11-19 22:10:14 +0100

    Use cordova camera plugin to open camera and gallery. Huge success.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,39 @@
-angular.module('symphonia.controllers', [])
+angular.module('symphonia.controllers', ['ngCordova'])
 
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $cordovaCamera, $ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      $scope.uploadPicture = function () {
+        var options = {
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+          //popoverOptions: CameraPopoverOptions
+        };
 
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+          var image = document.getElementById('myImage');
+          image.src = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+          // error
+        });
+      };
+
+      $scope.takeAPicture = function () {
+        var new_options = {
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA
+          //popoverOptions: CameraPopoverOptions
+        };
+
+        $cordovaCamera.getPicture(new_options).then(function (imageData) {
+          var image = document.getElementById('myImage');
+          image.src = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+          // error
+        });
+      }
+    });
   })
+
   .controller('OptionsCtrl', function ($scope, $ionicLoading, $timeout) {
     $scope.outputFormatList = [
       {text: 'Music XML', value: 'mxl'},
@@ -23,5 +54,4 @@ angular.module('symphonia.controllers', [])
         $ionicLoading.hide();
       }, 2000);
     };
-
   });
